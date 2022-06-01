ca20934d28e90c5620c826a8af6c35599245ff6b
kimho1007 <75024392+kimho1007@users.noreply.github.com>
2022-06-02 01:56:19 +0900

    Create PostTotal.java

---
+++ move0114/app/src/main/java/com/example/move0114/PostTotal.java
@@ -0,0 +1,79 @@
+package com.example.move0114;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.move0114.adapters.PostAdapter;
+import com.example.move0114.models.Post;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class PostTotal extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private FirebaseFirestore mStore = FirebaseFirestore.getInstance();
+
+    private RecyclerView mPostRecyclerView;
+
+    private PostAdapter mAdapter;
+    private List<Post> mDatas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_total);
+
+        mPostRecyclerView = findViewById(R.id.main_recyclerview);
+        findViewById(R.id.main_post_edit).setOnClickListener(this);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mDatas = new ArrayList<>();
+        mStore.collection(FirebaseID.post)
+                .orderBy(FirebaseID.timestamp, Query.Direction.DESCENDING)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                        if (value != null) {
+                            mDatas.clear();
+                            for (DocumentSnapshot snap : value.getDocuments()) {
+                                Map<String, Object> shot = snap.getData();
+                                String documentId = String.valueOf(shot.get(FirebaseID.documentId));
+                                String nickname = String.valueOf(shot.get(FirebaseID.nickname));
+                                String title = String.valueOf(shot.get(FirebaseID.title));
+                                String contents = String.valueOf(shot.get(FirebaseID.contents));
+                                Post data = new Post(documentId, nickname, title, contents);
+                                mDatas.add(data);
+                            }
+                            mAdapter = new PostAdapter(mDatas);
+                            mPostRecyclerView.setAdapter(mAdapter);
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View view) {
+        startActivity(new Intent(this, PostUse.class));
+
+
+    }
+}
